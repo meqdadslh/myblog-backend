2102013f952c55c5bc7ca89e13fa33966092108c
Meqdad Ali <meqdadali@Meqdads-MacBook-Pro.local>
2021-06-15 18:28:46 -0400

    run the crud function using the generic APIView and mixins

--- blog/views.py
+++ blog/views.py
@@ -7,112 +7,156 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import APIView
+from rest_framework import generics
+from rest_framework import mixins
 
 
 
+# ////////Generic APIView & Mixins///////FIRST AND BEST WAY////////
 
-class ArticleList(APIView):
+
+class ArticleList(generics.GenericAPIView,mixins.ListModelMixin, mixins.CreateModelMixin):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+    # Getting the list of all the articles
     def get(self, request):
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
-        return Response(serializer.data)
+        return self.list(request)
+
+    # Adding article 
+    def post(self, request):
+        return self.create(request)
 
 
-    def post(self,request):
-        serializer = ArticleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# Creating the specific model where you can look for an article by id, updating and removing the artiles. but first we need to add all the mixins inside the artcialedetails class
 
-# Adding put and delete functions
-class ArticleDetails(APIView):
+class ArticleDetails(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
 
-    def get_object(self, id):
-        try:
-            return Article.objects.get(id=id)
-        except Article.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    # GET a single article by the ID
-    
-    def get(self, request, id):
-        article = self.get_object(id)
-        serializer = ArticleSerializer(article)
-        return Response(serializer.data)
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
 
-    # Update the article 
+    # After running the details functions getting an error of using pk. we can fix that by adding the lookup_field=id
+    lookup_field = 'id'
 
+
+    # LOOK for an article by ID
+    def get(self,request,id):
+        return self.retrieve(request, id=id)
+
+
+    # UPDATING an article
     def put(self, request, id):
-        article = self.get_object(id)
-        serializer = ArticleSerializer(article, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return self.update(request, id=id)
 
-    # Delete the Article 
 
+    # DELETING and article
     def delete(self, request, id):
-        article = self.get_object(id)
-        article.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return self.destroy(request, id=id)
 
-    '''
-# Create your views here.
+# ///////////Class Based API Views///////SECOND WAY//////////
 
-
-@api_view (['GET', 'POST'])
-def article_list(request):
-
-# get the list of all articles
-
-    if request.method == 'GET':
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
-        return Response(serializer.data)
-
-# Creating an article ///////////////////
-
-    elif request.method == 'POST':
-        serializer = ArticleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# class ArticleList(APIView):
+#     def get(self, request):
+#         articles = Article.objects.all()
+#         serializer = ArticleSerializer(articles, many=True)
+#         return Response(serializer.data)
 
 
-# using rest apit
-@api_view (['GET', 'PUT', 'DELETE'])
+#     def post(self,request):
+#         serializer = ArticleSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-def article_details(request, pk):
-    try:
-        article = Article.objects.get(pk=pk)
+# # Adding put and delete functions
+# class ArticleDetails(APIView):
 
-    except Article.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+#     def get_object(self, id):
+#         try:
+#             return Article.objects.get(id=id)
+#         except Article.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
-        serializer = ArticleSerializer(article)
-        return Response(serializer.data)
+#     # GET a single article by the ID
+
+#     def get(self, request, id):
+#         article = self.get_object(id)
+#         serializer = ArticleSerializer(article)
+#         return Response(serializer.data)
+
+#     # Update the article 
+
+#     def put(self, request, id):
+#         article = self.get_object(id)
+#         serializer = ArticleSerializer(article, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     # Delete the Article 
+
+#     def delete(self, request, id):
+#         article = self.get_object(id)
+#         article.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-# UPDATING Article //////////////
 
-    elif request.method == 'PUT':
-        serializer = ArticleSerializer(article, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# DELETING AN ARTICLE///////////////////
-
-    elif request.method == 'DELETE':
-        article.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# ///////Function Bases API View////////THIRD WAY/////////////////////
 
 
-# now we are adding the routes to urls.py file ////////////////////
+# @api_view (['GET', 'POST'])
+# def article_list(request):
 
-    '''
+# # get the list of all articles
+
+#     if request.method == 'GET':
+#         articles = Article.objects.all()
+#         serializer = ArticleSerializer(articles, many=True)
+#         return Response(serializer.data)
+
+# # Creating an article ///////////////////
+
+#     elif request.method == 'POST':
+#         serializer = ArticleSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#             return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# # using rest apit
+# @api_view (['GET', 'PUT', 'DELETE'])
+
+# def article_details(request, pk):
+#     try:
+#         article = Article.objects.get(pk=pk)
+
+#     except Article.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+
+#     if request.method == 'GET':
+#         serializer = ArticleSerializer(article)
+#         return Response(serializer.data)
+
+
+# # UPDATING Article //////////////
+
+#     elif request.method == 'PUT':
+#         serializer = ArticleSerializer(article, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# # DELETING AN ARTICLE///////////////////
+
+#     elif request.method == 'DELETE':
+#         article.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# # now we are adding the routes to urls.py file ////////////////////
