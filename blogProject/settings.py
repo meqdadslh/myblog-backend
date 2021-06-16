ed468f950df1f2ae2cf48073121e2eb3f092c429
Meqdad Ali <meqdadali@Meqdads-MacBook-Pro.local>
2021-06-16 12:51:38 -0400

    added the pip lists for the heroku deployments

--- blogProject/settings.py
+++ blogProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 import rest_framework
 
 
@@ -133,5 +133,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
 
 
